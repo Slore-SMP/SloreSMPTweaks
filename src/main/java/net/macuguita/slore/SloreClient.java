e3bd3a0b491bfb84a7d83f2eab1c0675d31b08da
macuguita <raulpripri@gmail.com>
2024-12-09 02:02:28 +0100

    first commit

---
+++ src/main/java/net/macuguita/slore/SloreClient.java
@@ -0,0 +1,11 @@
+package net.macuguita.slore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SloreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/macuguita/slore/SloreSMPTweaks.java
@@ -0,0 +1,17 @@
+package net.macuguita.slore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SloreSMPTweaks implements ModInitializer {
+
+	public static final String MOD_ID = "slore-tweaks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
