956f8a6c9386ff4d2392d262efc20daeccb66165
macuguita <raulpripri@gmail.com>
2025-04-05 21:29:13 +0200

    particles!!!

--- src/main/java/com/macuguita/slore/client/SloreClient.java
+++ src/main/java/com/macuguita/slore/client/SloreClient.java
@@ -4,11 +4,12 @@ import com.macuguita.slore.SloreTweaks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.client.particle.CampfireSmokeParticle;
+import net.minecraft.client.particle.SoulParticle;
 
 public class SloreClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ParticleFactoryRegistry.getInstance().register(SloreTweaks.GHOST_PARTICLE, CampfireSmokeParticle.CosySmokeFactory::new);
+        ParticleFactoryRegistry.getInstance().register(SloreTweaks.GHOST_PARTICLE, SoulParticle.SculkSoulFactory::new);
     }
 }
